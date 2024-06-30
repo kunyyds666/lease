dfd0f03480bdabd79d2d057a9954a1426af9d32c
ChouChiKun <kunyyds666@gmail.com>
2024-06-30 13:10:27 +0800

    0.5

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/LoginServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/LoginServiceImpl.java
@@ -1,9 +1,85 @@
 package com.atguigu.lease.web.admin.service.impl;
 
+import com.atguigu.lease.common.constant.RedisConstant;
+import com.atguigu.lease.common.exception.LeaseException;
+import com.atguigu.lease.common.result.ResultCodeEnum;
+import com.atguigu.lease.common.utils.JwtUtil;
+import com.atguigu.lease.model.entity.SystemUser;
+import com.atguigu.lease.model.enums.BaseStatus;
+import com.atguigu.lease.web.admin.mapper.SystemUserMapper;
 import com.atguigu.lease.web.admin.service.LoginService;
+import com.atguigu.lease.web.admin.vo.login.CaptchaVo;
+import com.atguigu.lease.web.admin.vo.login.LoginVo;
+import com.wf.captcha.SpecCaptcha;
+import com.wf.captcha.base.Captcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.apache.commons.codec.digest.DigestUtils;
+
+
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class LoginServiceImpl implements LoginService {
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    @Autowired
+    private SystemUserMapper systemUserMapper;
+
+    @Override
+    public CaptchaVo getCaptcha() {
+        //创建图片验证码对象
+        SpecCaptcha specCaptcha = new SpecCaptcha(130,48,4);//验证码长宽和长度
+        specCaptcha.setCharType(Captcha.TYPE_DEFAULT);//default数字和字母混合
+
+        String code = specCaptcha.text().toLowerCase();//验证码不区分大小写，将验证码转化为全部小写，存入redis
+        String key = RedisConstant.ADMIN_LOGIN_PREFIX + UUID.randomUUID();
+        String image = specCaptcha.toBase64();//将验证图片转化为数据
+        redisTemplate.opsForValue().set(key, code, RedisConstant.ADMIN_LOGIN_CAPTCHA_TTL_SEC, TimeUnit.SECONDS);
+
+        return new CaptchaVo(image, key);
+    }
+
+    @Override
+    public String login(LoginVo loginVo) {
+        //1.判断是否输入了验证码
+        if (!StringUtils.hasText(loginVo.getCaptchaCode())) {
+            throw new LeaseException(ResultCodeEnum.ADMIN_CAPTCHA_CODE_NOT_FOUND);
+        }
+
+        //2.校验验证码
+        String code = redisTemplate.opsForValue().get(loginVo.getCaptchaKey());
+        if (code == null) {
+            throw new LeaseException(ResultCodeEnum.ADMIN_CAPTCHA_CODE_EXPIRED);
+        }
+
+        if (!code.equals(loginVo.getCaptchaCode().toLowerCase())) {
+            throw new LeaseException(ResultCodeEnum.ADMIN_CAPTCHA_CODE_ERROR);
+        }
+
+        //3.校验用户是否存在
+        SystemUser systemUser = systemUserMapper.selectOneByUsername(loginVo.getUsername());
+
+        if (systemUser == null) {
+            throw new LeaseException(ResultCodeEnum.ADMIN_ACCOUNT_NOT_EXIST_ERROR);
+        }
+
+        //4.校验用户是否被禁
+        if (systemUser.getStatus() == BaseStatus.DISABLE) {
+            throw new LeaseException(ResultCodeEnum.ADMIN_ACCOUNT_DISABLED_ERROR);
+        }
+
+        //5.校验用户密码
+        if (!systemUser.getPassword().equals(DigestUtils.md5Hex(loginVo.getPassword()))) {
+            throw new LeaseException(ResultCodeEnum.ADMIN_ACCOUNT_ERROR);
+        }
+
+        //6.创建并返回TOKEN
+        return JwtUtil.createToken(systemUser.getId(), systemUser.getUsername());
+    }
 }
